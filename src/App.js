e08bc114e788647962f5366ea9f3d6390f5e4f38
maximkwski <makkie.makowski@gmail.com>
2022-08-15 13:35:48 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePage from "./HomePage";
+
+function App() {
+  return (
+    <>   
+      <HomePage />
+      <input type="text" />
+    </>   
+  )
+}
+
+export default App;
