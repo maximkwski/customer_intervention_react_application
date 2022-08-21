18c0bd40fc7bb95c53fd2fc96cae67cef7501fff
maximkwski <makkie.makowski@gmail.com>
2022-08-21 18:03:29 -0400

    route added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Dashboard from "./components/Dashboard";
-import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate} from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import InterventionForm from "./InterventionForm";
 import axios from "axios";
@@ -13,6 +13,10 @@ function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/intervention" element={<InterventionForm />} />
+        <Route
+        path="*"
+        element={<Navigate to="/login" replace />}
+    />
     </Routes>
    </BrowserRouter>
    );
